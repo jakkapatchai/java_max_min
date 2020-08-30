c2d2bb05bcb08bc458f54b84f2cf3ac259af74d0
jakkapatchai <45837555+jakkapatchai@users.noreply.github.com>
2020-08-31 04:09:14 +0700

    Add files via upload

---
+++ 1.1.java
@@ -0,0 +1,12 @@
+class x {
+  public static void main(String args[]) {
+    int c1 = Integer.parseInt(args[0]);
+    int c2 = Integer.parseInt(args[1]);
+    int c3 = Integer.parseInt(args[2]);
+    int c4 = Integer.parseInt(args[3]);
+    System.out.println("c1" + " + " + "c2" + " + " + "c3" + " + " + "c4" + " = " + (c1 + c2 + c3 + c4));
+    System.out.println("c1" + " - " + "c2" + " - " + "c3" + " - " + "c4" + " = " + (c1 - c2 - c3 - c4));
+    System.out.println("c1" + " * " + "c2" + " * " + "c3" + " * " + "c4" + " = " + (c1 * c2 * c3 * c4));
+    System.out.println("c1" + " / " + "c2" + " / " + "c3" + " / " + "c4" + " = " + (c1 / c2 / c3 / c4));
+  }
+}
---
+++ 1.2.java
@@ -0,0 +1,20 @@
+class x {
+  public static void main(String args[]) {
+    byte c1 = 1;
+    short c2 = 2;
+    int c3 = 3;
+    long c4 = 4;
+    float c5 = 5f;
+    double c6 = 6d;
+    boolean c7 = true;
+    char c8 = 'c';
+    System.out.println(c1+c2+c3+c4+c5+c6+c8);
+    System.out.println(c2-c1);
+    System.out.println(c3*c4);
+    System.out.println(c4/c2);
+    System.out.println(c5+c6+c8);
+    System.out.println(c8+2);
+    System.out.println(c7);
+    System.out.println(c6/c3);
+  }
+}
